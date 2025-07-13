57a1f8cecf83894bf2db21ce20d5a8abe7d006b6
Blue Dress <stra6aro@abv.bg>
2025-07-13 19:22:25 +0300

    added Majority Element

---
+++ ArrayOrString/MajorityElement.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercises.ArrayOrString
+{
+    public static class MajorityElement
+    {
+        public static int Solution(int[] nums)
+        {
+            var elementsCount = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (elementsCount.ContainsKey(nums[i]))
+                {
+                    elementsCount[nums[i]]++;
+                }
+                else
+                {
+                    elementsCount[nums[i]] = 1;
+                }
+
+                if (elementsCount[nums[i]] > nums.Length / 2)
+                {
+                    return nums[i];
+                }
+            }
+
+            return elementsCount.OrderByDescending(e => e.Value).First().Key; 
+        }
+    }
+}
