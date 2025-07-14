93b9e1015f236f13141c2abbaafca1afeec72588
Blue Dress <stra6aro@abv.bg>
2025-07-14 20:23:50 +0300

    added Rotate Array

---
+++ ArrayOrString/RotateArray.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercises.ArrayOrString
+{
+    public static class RotateArray
+    {
+        public static void Solution(int[] nums, int k)
+        {
+            var rotatedNums = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i + k % nums.Length < nums.Length)
+                {
+                    rotatedNums[i + k % nums.Length] = nums[i];
+                }
+                else
+                {
+                    rotatedNums[i + k % nums.Length - nums.Length] = nums[i];
+                }
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rotatedNums[i];
+            }
+        }
+    }
+}
