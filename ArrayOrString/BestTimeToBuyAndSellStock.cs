11ea26fad595c1ba5019ff68b08fe36f8b6a4139
Blue Dress <stra6aro@abv.bg>
2025-07-15 21:00:21 +0300

    added Best Time to Buy and Sell Stock

---
+++ ArrayOrString/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercises.ArrayOrString
+{
+    public static class BestTimeToBuyAndSellStock
+    {
+        public static int Solution(int[] prices)
+        {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                minPrice = Math.Min(minPrice, prices[i]);
+                maxProfit = Math.Max(maxProfit, prices[i] - minPrice);
+            }
+
+            return maxProfit;
+        }
+    }
+}
