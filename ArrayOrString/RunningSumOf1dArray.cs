8c35a770f5c062ced72b52e4f3f2aa8ad25c826b
Blue Dress <stra6aro@abv.bg>
2025-07-17 20:36:43 +0300

    added Running Sum of 1d Array

---
+++ ArrayOrString/RunningSumOf1dArray.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercises.ArrayOrString
+{
+    public static class RunningSumOf1dArray
+    {
+        public static int[] Solution(int[] nums)
+        {
+            int currentSum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                currentSum = nums[i] + currentSum;
+                nums[i] = currentSum;
+            }
+
+            return nums;
+        }
+    }
+}
