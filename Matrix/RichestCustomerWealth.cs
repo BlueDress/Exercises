b8f2611a46034e56e4fbb209f2f59b7df9f3d93c
Blue Dress <stra6aro@abv.bg>
2025-07-18 18:05:06 +0300

    added Richest Customer Wealth

---
+++ Matrix/RichestCustomerWealth.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercises.Matrix
+{
+    public static class RichestCustomerWealth
+    {
+        public static int Solution(int[][] accounts)
+        {
+            var richestWealth = 0;
+
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                var currentWealth = 0;
+
+                for (int j = 0; j < accounts[i].Length; j++)
+                {
+                    currentWealth += accounts[i][j];
+                }
+
+                if (currentWealth > richestWealth)
+                {
+                    richestWealth = currentWealth;
+                }
+            }
+
+            return richestWealth;
+        }
+    }
+}
