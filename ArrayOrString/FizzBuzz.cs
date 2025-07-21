86c01e908b6d43b490d6d47a6cd54db226b4061a
Blue Dress <stra6aro@abv.bg>
2025-07-21 18:17:06 +0300

    added Fizz Buzz

---
+++ ArrayOrString/FizzBuzz.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercises.ArrayOrString
+{
+    public static class FizzBuzz
+    {
+        public static IList<string> Solution(int n)
+        {
+            var answer = new List<string>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    answer.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    answer.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    answer.Add("Buzz");
+                }
+                else
+                {
+                    answer.Add(i.ToString());
+                }
+            }
+
+            return answer;
+        }
+    }
+}
